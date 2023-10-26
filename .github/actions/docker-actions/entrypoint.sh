83bb0369c5be9037bf651b44e1e1d219882f3a41
Chab <chabane.hamma@gmail.com>
2023-10-26 16:03:53 +0200

    commit

--- .github/actions/docker-actions/entrypoint.sh
+++ .github/actions/docker-actions/entrypoint.sh
@@ -3,4 +3,9 @@
 echo "Hello $1"
 time=$(date)
 echo "time=$time" >> $GITHUB_OUTPUT
-echo "coucou" >> /github/workspace/coucou.txt
+# Write any data in a file so that the calling workflow can retrieve it as follows
+#       - name: Get the file created in the docker entrypoint.sh
+#         run: |
+#           cd $GITHUB_WORKSPACE
+#           cat result.txt
+echo "my result value" >> /github/workspace/result.txt
