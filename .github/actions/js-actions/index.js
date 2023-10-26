53f940e24ad8cf435afbc287b7fe0867654d89b5
Chab <chabane.hamma@gmail.com>
2023-10-26 15:32:45 +0200

    commit

--- .github/actions/js-actions/index.js
+++ .github/actions/js-actions/index.js
@@ -11,7 +11,7 @@ function run () {
         core.setOutput("time", time);
         // Get the JSON webhook payload for the event that triggered the workflow
         const payload = JSON.stringify(github.context.payload, undefined, 2)
-        console.log(`The event payload: ${payload}`);
+        exec.exec(`echo : "${payload}"`);
     } catch (error) {
         core.setFailed(error.message);
     }
